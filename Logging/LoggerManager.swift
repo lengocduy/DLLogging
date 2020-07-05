5bcaa962c8cf31183f8c33c78831e18ff2d7dace
Le Ngoc Duy <askbills@gmail.com>
2020-07-06 01:32:10 +0700

    chore(skeleton): add place holder files as diagram

---
+++ Logging/LoggerManager.swift
@@ -0,0 +1,9 @@
+//
+//  LoggerManager.swift
+//  Logging
+//
+//  Created by Duy Le Ngoc on 7/6/20.
+//  Copyright © 2020 Duy Le Ngoc. All rights reserved.
+//
+
+import Foundation
---
+++ Logging/Model/ReadWriteLock.swift
@@ -0,0 +1,9 @@
+//
+//  ReadWriteLock.swift
+//  Logging
+//
+//  Created by Duy Le Ngoc on 7/6/20.
+//  Copyright © 2020 Duy Le Ngoc. All rights reserved.
+//
+
+import Foundation
