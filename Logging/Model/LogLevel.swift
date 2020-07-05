f400ec48b741092d1b97194d02c304e94d650b3c
Le Ngoc Duy <askbills@gmail.com>
2020-07-06 01:37:55 +0700

    feat(model): add separated models

--- Logging/Model/LogLevel.swift
+++ Logging/Model/LogLevel.swift
@@ -7,3 +7,41 @@
 //
 
 import Foundation
+
+/// This defines the various levels of logging that a message may be tagged with. This allows hiding and
+/// showing different logging levels at run time depending on the environment
+// MARK: - LogLevel
+public enum LogLevel: String {
+    /// Verbose 🗣 - A verbose message, usually useful when working on a specific problem
+    case verbose
+    
+    /// Debug 🔍 - A debug message that may be useful to a developer
+    case debug
+
+    /// Info ℹ️ - An info message that highlight the progress of the application at coarse-grained level.
+    case info
+
+    /// Warning ⚠️ - A warning message, may indicate a possible error
+    case warning
+
+    /// Error ❗️ - An error occurred, but it's recoverable, just info about what happened
+    case error
+    
+    /// Severe 🛑 - A server error occurred
+    case severe
+    
+    var symbol: String {
+        switch self {
+        case .verbose : return "🗣"
+        case .debug : return "🔍"
+        case .info : return "ℹ️"
+        case .warning : return "⚠️"
+        case .error : return "❗️"
+        case .severe : return "🛑"
+        }
+    }
+    
+    var name: String {
+        return "[\(self.rawValue.uppercased())]"
+    }
+}
--- Logging/Model/LogMessage.swift
+++ Logging/Model/LogMessage.swift
@@ -7,3 +7,41 @@
 //
 
 import Foundation
+
+/// This holds all the data for each log message, since the formatting is up to each
+/// logging object. It is a simple bag of data
+// MARK: - LogMessage
+public struct LogMessage {
+    /// The file where this log message was created
+    let file: String
+
+    /// The function where this log message was created
+    let function: String
+
+    /// The text of the log message
+    let text: String
+
+    /// The level of the log message
+    let level: LogLevel
+    
+    /// The line of the log message
+    let line: Int
+    
+    /// The value to know whether need to sync with other system or not
+    let isAllowedSyncToCloud: Bool
+
+    init(path: String, function: String,
+         text: String, level: LogLevel,
+         line: Int, isAllowedSyncToCloud: Bool = false) {
+        if let file = path.components(separatedBy: "/").last {
+            self.file = file
+        } else {
+            self.file = path
+        }
+        self.function = function
+        self.text = text
+        self.level = level
+        self.line = line
+        self.isAllowedSyncToCloud = isAllowedSyncToCloud
+    }
+}
